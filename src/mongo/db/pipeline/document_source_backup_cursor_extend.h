033b61bd5746576e48a18dd699817777b78a25a7
Igor Solodovnikov <igor.solodovnikov@percona.com>
2024-02-02 20:08:54 +0000

    PSMDB-1284 specify read concern capability of $backupCursor & $backupCursorExtend
    When something is called by internal client read concern must be
    explicitly specified.

--- src/mongo/db/pipeline/document_source_backup_cursor_extend.h
+++ src/mongo/db/pipeline/document_source_backup_cursor_extend.h
@@ -62,7 +62,12 @@ public:
             return true;
         }
 
-        void assertSupportsMultiDocumentTransaction() const {
+        ReadConcernSupportResult supportsReadConcern(repl::ReadConcernLevel level,
+                                                     bool isImplicitDefault) const final {
+            return onlyReadConcernLocalSupported(kStageName, level, isImplicitDefault);
+        }
+
+        void assertSupportsMultiDocumentTransaction() const final {
             transactionNotSupported(kStageName);
         }
     };

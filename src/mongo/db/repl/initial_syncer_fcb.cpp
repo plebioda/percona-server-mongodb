d31f0c31c02d50493d55c2e0f38ee8805579591c
Igor Solodovnikov <igor.solodovnikov@percona.com>
2024-06-24 16:16:07 +0100

    PSMDB_1284 kill local backup cursor

--- src/mongo/db/repl/initial_syncer_fcb.cpp
+++ src/mongo/db/repl/initial_syncer_fcb.cpp
@@ -1359,6 +1359,9 @@ StatusWith<std::vector<std::string>> InitialSyncerFCB::_getBackupFiles() {
             auto rec = cursor->next();
             files.emplace_back(rec[kFileNameFieldName].String());
         }
+
+        // Close cursor
+        cursor->kill();
     } catch (const DBException& e) {
         return e.toStatus();
     }

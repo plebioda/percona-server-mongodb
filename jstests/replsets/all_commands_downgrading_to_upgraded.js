93a367ec6f2a517fb7b81ef694f599c16fb64a5c
Konstantin Trushin <konstantin.trushin@percona.com>
2024-07-04 11:38:52 +0400

    PSMDB-1395 Add missing `auditGetOptions` and `createBackup` commands to the test (#1267)
    * PSMDB-1395 added missing auditGetOptions and createBackup commands to the test
    
    (cherry picked from commit e70f7f456768cf0e08b408ec90a0e18feb2e678d)
    
    * PSMDB-1395 changed skip message for createBackup command, added execution of auditGetOptions command
    
    (cherry picked from commit 26ffddb0d7c4f4c6083f4836f73ff0737a8422c7)
    
    ---------
    
    Co-authored-by: Oleksandr Havryliak <oleksandr.havryliak@percona.com>

--- jstests/replsets/all_commands_downgrading_to_upgraded.js
+++ jstests/replsets/all_commands_downgrading_to_upgraded.js
@@ -270,6 +270,9 @@ const allCommands = {
         isAdminCommand: true,
         isShardSvrOnly: true,
     },
+    auditGetOptions: {
+        command: {auditGetOptions: 1},
+    },
     authenticate: {
         // Skipping command because it requires additional authentication setup.
         skip: "requires additional authentication setup"
@@ -582,6 +585,7 @@ const allCommands = {
             assert.commandWorked(conn.getDB(dbName).runCommand({drop: collName}));
         },
     },
+    createBackup: {skip: "requires additional setup to execute backup"},
     createIndexes: {
         setUp: function(conn) {
             assert.commandWorked(conn.getDB(dbName).runCommand({create: collName}));

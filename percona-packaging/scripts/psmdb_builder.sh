ebe75ce1c999f1f103402664014eeed1b0daf73f
Sandra Romanchenko <53295797+sandraromanchenko@users.noreply.github.com>
2025-06-05 08:51:08 +0300

    PSMDB-1682. Update mongo tools version to 100.12.1, v2 (#1481)

--- percona-packaging/scripts/psmdb_builder.sh
+++ percona-packaging/scripts/psmdb_builder.sh
@@ -182,7 +182,7 @@ get_sources(){
     # Dirty hack for mongo-tools 100.7.3 and aarch64 builds. Should fail once Mongo fixes OS detection https://jira.mongodb.org/browse/TOOLS-3318
     # Use rhel93 selector for all builds, no fix by this time(20.01.2025)
 #    if [ x"$ARCH" = "xaarch64" ]; then
-        sed -i '131 {/\(GetByOsAndArch("ubuntu1804", archName)\)/ s/\bubuntu1804\b/rhel93/; t; q1}' release/platform/platform.go || exit 1
+        sed -i '/Arch:[[:space:]]*ArchArm64/s/ArchArm64/ArchAarch64/' release/platform/platform.go || exit 1
 #    fi
 
     cd ${WORKDIR}

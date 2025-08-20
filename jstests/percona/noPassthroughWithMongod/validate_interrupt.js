215a6896489ed6fc4ea3949c696dfc8af2f148ac
Pawel Lebioda <pawel.lebioda@percona.com>
2025-08-20 22:18:50 +0200

    PSMDB-1674: fix validate_interrupt.js test
    The second warning does not match the regex.
    It indicates warning for the index.
    The index itself is in a dedicated field for warnings for the index.

--- jstests/percona/noPassthroughWithMongod/validate_interrupt.js
+++ jstests/percona/noPassthroughWithMongod/validate_interrupt.js
@@ -38,9 +38,14 @@
         assert((numWarnings === 1) || (numWarnings === 2),
                'Expected 1 or 2 validation warnings:\n' + tojson(res));
         const re = new RegExp("Could not complete validation of table:(collection|index)([0-9\-]+). This is a transient issue as the collection was actively in use by other operations.");
-        assert(res.warnings[0].match(re), 'Expected a transient issue warning:\n' + tojson(res));
+            assert(res.warnings[0].match(re), 'Expected a transient issue warning:\n' + tojson(res));
         if (numWarnings === 2) {
-            assert(res.warnings[1].match(re), 'Expected a transient issue warning:\n' + tojson(res));
+            assert(res.warnings[1] === "Found warnings in _id_",
+                   'Expected warning for _id_ index:\n' + tojson(res));
+            assert(res.indexDetails._id_.warnings.length === 1,
+                   'Expected 1 warning for _id_ index:\n' + tojson(res));
+            assert(res.indexDetails._id_.warnings[0].match(re),
+                   'Expected a transient issue warning:\n' + tojson(res));
         }
     }
 })();

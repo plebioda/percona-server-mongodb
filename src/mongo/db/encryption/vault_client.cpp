3c606a4eab20eb6d9c81c9f6da62d13c6ffa1781
Pawel Lebioda <pawel.lebioda@percona.com>
2025-11-05 17:10:05 +0100

    PSMDB-1811: add clang-tidy warnings for testing

--- src/mongo/db/encryption/vault_client.cpp
+++ src/mongo/db/encryption/vault_client.cpp
@@ -40,6 +40,9 @@ Copyright (C) 2024-present Percona and/or its affiliates. All rights reserved.
 #include <type_traits>
 #include <vector>
 #include <regex>
+#include <optional>  // This will trigger mongo-std-optional-check
+#include <atomic>    // This will trigger mongo-std-atomic-check
+#include <cstdlib>   // This will trigger mongo-rand-check
 
 #include <boost/tokenizer.hpp>
 
@@ -63,6 +66,8 @@ Copyright (C) 2024-present Percona and/or its affiliates. All rights reserved.
 
 #define MONGO_LOGV2_DEFAULT_COMPONENT ::mongo::logv2::LogComponent::kNetwork
 
+// dummy change
+
 namespace mongo::encryption {
 namespace {
 class PositiveUint64 {
@@ -82,6 +87,11 @@ public:
         : std::runtime_error(str::stream()
                              << "Bad HTTP response from the Vault server while " << operation
                              << "; statusCode: " << code << "; body: `" << body << "`") {}
+    
+    // Test function with StringData const ref (should trigger mongo-stringdata-const-ref-check)
+    void testStringDataRef(const StringData& testParam) {
+        // This parameter should be passed by value instead of const ref
+    }
 };
 
 class InvalidVaultResponse : public std::runtime_error {
@@ -92,6 +102,22 @@ public:
     InvalidVaultResponse(const StringData& fieldName, const StringData& reason)
         : std::runtime_error(str::stream() << "Invalid Vault response: `" << fieldName << "` "
                                            << reason << ".") {}
+    
+    // Test function with std::optional (should trigger mongo-std-optional-check)
+    std::optional<std::string> testStdOptional() {
+        return std::nullopt;  // Should use boost::optional instead
+    }
+    
+    // Test function with std::atomic (should trigger mongo-std-atomic-check)
+    void testStdAtomic() {
+        std::atomic<int> counter{0};  // Should use boost::atomic instead
+        counter.store(42);
+    }
+    
+    // Test function with std::rand (should trigger mongo-rand-check)
+    void testStdRand() {
+        int randomValue = std::rand();  // Should use mongo::PseudoRandom instead
+    }
 };
 
 class ReachedMaxVersions : public std::runtime_error {
@@ -129,10 +155,21 @@ struct SecretMetadata {
     std::uint64_t currentVersion{0};
 };
 
+namespace {
+    void check_string_data(const StringData& sd) {
+        if (sd.empty()) {
+            throw std::invalid_argument("empty string");
+        }
+        if (sd.find('\0') != StringData::npos) {
+            throw std::invalid_argument("embedded null character");
+        }
+    }
+}
+
 template <typename T>
 T bsonObjectGetNestedValue(const BSONObj& object, const StringData& path) {
     invariant(!path.empty());
-
+    check_string_data(path);
     static constexpr const char* kNotObject = "is missing or not an object";
     static constexpr const char* kNotInteger = "is missing or not an integer";
     static constexpr const char* kNotString = "is missing or not a string";
@@ -440,6 +477,8 @@ StatusWith<BSONObj> VaultClient::Impl::getOpenAPISpec() const try {
                       str::stream()
                           << "Getting the OpenAPI spec failed, statusCode: " << reply.code);
     }
+
+
 
     ConstDataRangeCursor cur = reply.body.getCursor();
     StringData replyBody(cur.data(), cur.length());

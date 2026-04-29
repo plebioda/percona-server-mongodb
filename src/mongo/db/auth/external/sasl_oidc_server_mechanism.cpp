2b4b5c89b30037d3f13c2620e885fd24ddd100ed
Pawel Lebioda <pawel.lebioda@percona.com>
2026-04-29 16:00:40 +0200

    Fix clang-tidy failure for merge commit 'aea0c37039a' into master
    Fixed Percona-specific code in sasl_oidc_server_mechanism.cpp that
    referenced a non-existent header (bson_time_support.h) and undefined
    function (parseDateFromDurationSinceEpoch). Root cause: Percona-specific
    code was using incorrect API. Changed to use the correct MongoDB
    crypto/unix_epoch.h header and parseUnixEpoch function.
    
    Resolved:
    	src/mongo/db/auth/external/sasl_oidc_server_mechanism.cpp
    
    CI: clang-tidy run 25095788526 (attempt 1 of 2)
    
    Note: commit created by mergai

--- src/mongo/db/auth/external/sasl_oidc_server_mechanism.cpp
+++ src/mongo/db/auth/external/sasl_oidc_server_mechanism.cpp
@@ -31,7 +31,7 @@ Copyright (C) 2025-present Percona and/or its affiliates. All rights reserved.
 
 #include "mongo/db/auth/external/sasl_oidc_server_mechanism.h"
 
-#include "mongo/bson/bson_time_support.h"
+#include "mongo/crypto/unix_epoch.h"
 #include "mongo/bson/bsonobj.h"
 #include "mongo/crypto/jws_validated_token.h"
 #include "mongo/db/auth/oidc/oidc_identity_providers_registry.h"
@@ -54,7 +54,7 @@ constexpr const char* kAuthTimeFieldName = "auth_time";
 boost::optional<Date_t> tokenGetPastDate(const crypto::JWSValidatedToken& token,
                                          StringData fieldName) {
     if (auto elem{token.getBodyBSON().getField(fieldName)}; elem.ok()) {
-        Date_t d{parseDateFromDurationSinceEpoch<Seconds>(elem)};
+        Date_t d{crypto::parseUnixEpoch(elem)};
         // The value is copied from the `JWSValidatedToken::validate` method
         // (please @see the `kNotBeforeSkewMax` variable there)
         constexpr Seconds kMaxClockSkew{60};
